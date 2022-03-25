d10a15861ac59fe26ddb2c3d04dab32f1f99ab70
Temperest <chawanwit.jrssw@gmail.com>
2022-03-25 14:14:48 +0700

    Create function for playing rock-paper-scissors game     in javascript.js file

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Rock-Paper-Scissors
+	First projecct
+	Use javascript to build Rock-Paper-Scissors play with bot 5 round
+		Demonstrate String Number Operator Condition and  Function
---
+++ javascript.js
@@ -0,0 +1,56 @@
+// function for computer randomly get Paper Scissors and Rock 
+function computerPlay() {
+    let randomNumber = Math.floor(Math.random()*3)+1;
+    let result;
+    if (randomNumber === 3){
+        result = "Scissors";
+    } else if(randomNumber === 2){
+        result = "Paper";
+    } else if(randomNumber === 1){
+        result = "Rock";
+    } else {
+        result = "Try again";
+    }
+    return result
+}
+
+function playRound(playerSelection,computerSelection){
+
+    let result;
+    if ((playerSelection === "Rock") && (computerSelection === "Paper")){
+        result = console.log("You Lose! Paper beats Rocks");
+    }   // Paper and Rock   = bot win
+    else  if ((playerSelection === "Rock") && (computerSelection === "Scissors")){
+        result = console.log("You Win! Rock beats Scissors");
+    }  // Rock and Scissors =  player win
+    else  if ((playerSelection === "Rock") && (computerSelection === "Rock")){
+        result = console.log("Draws");
+    }  // rock and rock     =  draw
+    else  if ((playerSelection === "Scissors") && (computerSelection === "Paper")){
+        result = console.log("You Win! Scissors beats Paper");
+    } // Scissors and Paper = player win
+    else  if ((playerSelection === "Scissors") && (computerSelection === "Scissors")){
+        result = console.log("Draw");
+    } // Scissors and Scissors = draw
+    else  if ((playerSelection === "Scissors") && (computerSelection === "Rock")){
+        result = console.log("You Lose! Rock beats Scissors");
+    } // Scissors and Rock  = bot win
+    else  if ((playerSelection === "Paper") && (computerSelection === "Paper")){
+        result = console.log("Draw");
+    } // Paper and Paper    = Draw 
+    else  if ((playerSelection === "Paper") && (computerSelection === "Scissors")){
+        result = console.log("You Lose! Scissors beats Paper");
+    } // Paper and Scissors = bot win
+    else  if ((playerSelection === "Paper") && (computerSelection === "Rock")){
+        result = console.log("You Win! Paper beats Rocks");
+    } // Paper and Rock     = player win
+    else{
+        result = console.log("Select your output");
+    }
+
+}
+playerSelection = "No";
+computerSelection = computerPlay();
+console.log(computerSelection);
+
+console.log(playRound(playerSelection, computerSelection));
